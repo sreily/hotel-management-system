abfbcf9811a8af325cd2b9a81d6fa53bd8348f85
sreily <reiclemente2005@gmail.com>
2025-04-27 17:44:13 +0300

    added functional links

--- app.py
+++ app.py
@@ -1,20 +1,55 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 from hotel import HotelManagementSystem
 
 app = Flask(__name__)
-hotel = HotelManagementSystem()
+app.secret_key = 'vgtucomputerengineeringekfu24/1'
+
+users = {}
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect('/')
+        else:
+            return "Login failed! Wrong username or password."
+    return render_template('login.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        users[username] = password
+        return redirect('/login')
+    return render_template('register.html')
 
 @app.route('/')
 def home():
-    available_rooms = hotel.show_available_rooms()
-    return render_template('index.html', rooms=available_rooms)
+    if 'username' not in session:
+        return redirect('/login')
+    return render_template('index.html')
 
-@app.route('/book', methods=['POST'])
-def book():
-    name = request.form['name']
-    room_type = request.form['room_type']
-    hotel.book_room(name, room_type)
-    return redirect('/')
+@app.route('/rooms')
+def rooms():
+    if 'username' not in session:
+        return redirect('/login')
+    return render_template('rooms.html')
+
+@app.route('/reservations')
+def reservations():
+    if 'username' not in session:
+        return redirect('/login')
+    return render_template('reservations.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect('/login')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
